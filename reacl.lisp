075ab883c09ea4437f060cd1efc8ae516275deef
bnmcgn@gmail.com <bnmcgn@gmail.com>
2021-01-26 11:15:23 -0800

    Context bugfixes

--- reacl.lisp
+++ reacl.lisp
@@ -20,10 +20,10 @@
 (defpsmacro defcontext (name value &key display-name)
   `(progn
      (var ,name (react create-context ,value))
-     (when ,display-name (setf (@ ,name display-name) ,display-name))))
+     (setf (@ ,name display-name) ,(or display-name name))))
 
 (defpsmacro let-context ((&rest context-pairs) &body body)
-  (let ((accum `(progn ,@body)))
+  (let ((accum `(list ,@body)))
     (dolist (itm (reverse context-pairs))
       (unless (listp itm)
         (error "Contexts must be provided in (context value) pairs"))
@@ -35,14 +35,14 @@
     accum))
 
 (defpsmacro get-context ((&rest context-bindings) &body body)
-  (let ((accum body))
+  (let ((accum `(list ,@body)))
     (dolist (itm (reverse context-bindings))
       (let ((context (if (listp itm) (car itm) itm))
             (binding (if (listp itm) (second itm) itm)))
         (setf
          accum
          `(react create-element (@ ,context -consumer)
-                 {}
-                 (=> ,binding (,accum))))))
+                 (create :key ,(format nil "~a-key" context))
+                 (paren6:=> ,binding ,accum)))))
     accum))
 
